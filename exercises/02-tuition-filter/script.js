fa24da8fa7d30096d135a775a4a15623029e8371
Saeed Wumbei Mamani <228374580+Section33-ops@users.noreply.github.com>
2026-05-27 22:49:46 -0400

    feat: practice filter() and map() methods. also learnt ternary operators

--- exercises/02-tuition-filter/script.js
+++ exercises/02-tuition-filter/script.js
@@ -18,4 +18,45 @@ function processTuition(roster) {
   console.log(`Action required: ${actionRequired}`);
 }
 
-processTuition(studentRoster);
+// processTuition(studentRoster);
+
+// const studentHasGrant = studentRoster.filter((student) => student.hasGrant);
+// console.log(studentHasGrant);
+
+// // Debt check
+// const studentOwes = studentRoster.filter((student) => student.balance >= 4500);
+// console.log(studentOwes);
+
+// //Self funded students
+// const SelfFundedStudent = studentRoster.filter((student) => !student.hasGrant);
+// console.log(SelfFundedStudent);
+
+// //High risk list
+// const highRiskStudents = studentRoster.filter(
+//   (student) => !student.hasGrant && student.balance >= 4500,
+// );
+// console.log(highRiskStudents);
+
+// // Name tag generator
+// const studentNameTags = studentRoster.map((student) => student.name);
+
+// console.log(studentNameTags);
+
+// // Tuition Hike
+// const studentTuitionIncrease = studentRoster.map((student) => ({
+//   ...student,
+//   balance: student.balance * 1.1,
+// }));
+// console.log(studentTuitionIncrease);
+
+// // Grant bonus
+// const grantBonus = studentRoster.map((student) =>
+//   student.hasGrant ? { ...student, balance: student.balance - 500 } : student,
+// );
+// console.log(grantBonus);
+
+// Collection Notice
+const collectNoticeForStudents = studentRoster
+  .filter((student) => student.balance > 4000)
+  .map((student) => `Urgent: ${student.name}, please pay your balance`);
+console.log(collectNoticeForStudents);
